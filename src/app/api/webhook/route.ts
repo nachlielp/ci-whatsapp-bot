412555e8f93adfbd0bc89d83f46054cb7763b265
Nachliel <nachpok@gmail.com>
2025-04-04 21:05:39 +0300

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { supabase } from "../../Supabase";
 import { tryCatch } from "@/util/tryCatch";
-export async function GET(request: Request) {
-  const result = await tryCatch(supabase.test({ request: request }));
+
+export async function POST(request: Request) {
+  const body = await tryCatch(request.json());
+  const result = await tryCatch(supabase.test({ body: body.data }));
   if (result.error) {
     // TODO: handle error sentry
     return NextResponse.json({ status: "error" });
