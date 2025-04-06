2ed6bd415b4e77c0f97bd66b66fe47076ba2ff5a
Nachliel <nachpok@gmail.com>
2025-04-06 22:05:02 +0300

    cleanup

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,22 +4,22 @@ import { supabase } from "@/app/Supabase";
 import { districtOptions } from "../interface";
 
 export async function GET() {
-  const user = await supabase.getUserByPhoneNumber("972584994306");
+  // const user = await supabase.getUserByPhoneNumber("972584994306");
 
-  const weeklyFilter = user.filter.map((r) => {
-    return districtOptions.find((d) => d.value === r)?.label;
-  });
+  // const weeklyFilter = user.filter.map((r) => {
+  //   return districtOptions.find((d) => d.value === r)?.label;
+  // });
 
-  const res = await twilio.sendTemplate(
-    "whatsapp:+972584994306",
-    "HXa0da0d6c5155a697326df97383b79b28",
-    {
-      "1": "נחליאל",
-      "2": weeklyFilter.join(", "),
-      "3": weeklyFilter.length.toString(),
-    }
-  );
-  console.log("res", res);
+  // const res = await twilio.sendTemplate(
+  //   "whatsapp:+972584994306",
+  //   "HXa0da0d6c5155a697326df97383b79b28",
+  //   {
+  //     "1": "נחליאל",
+  //     "2": weeklyFilter.join(", "),
+  //     "3": weeklyFilter.length.toString(),
+  //   }
+  // );
+  // console.log("res", res);
 
   throw new Error("test error blabla");
   return NextResponse.json({});
