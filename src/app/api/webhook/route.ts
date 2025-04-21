cb2bcb4268bc5ee4085d45774b36904164c5dd8b
Nachliel <nachpok@gmail.com>
2025-04-21 17:04:50 +0300

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -16,12 +16,12 @@ export async function POST(request: Request) {
   const startTime = Date.now(); // Capture start time
   let twilioResult;
 
-  const isValid = await twilio.validateTwilioRequest(request);
+  // const isValid = await twilio.validateTwilioRequest(request);
 
-  if (!isValid) {
-    // throw new Error(`Invalid request ${JSON.stringify(request)}`);
-    console.log("api/webhook/route.ts: Invalid request", request);
-  }
+  // if (!isValid) {
+  //   // throw new Error(`Invalid request ${JSON.stringify(request)}`);
+  //   console.log("api/webhook/route.ts: Invalid request", request);
+  // }
   try {
     // Parse the request body
     const formData = await request.formData();
